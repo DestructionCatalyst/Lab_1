32f1cfcf21a91ee35f69a02e03938f7886ffe96e
Владислав Комза <komza.vlad@ya.ru>
2020-02-22 12:23:36 +0300

    Added something

--- Lab_1/Source.c
+++ Lab_1/Source.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 #include <locale.h>
+#define FormType(a) a->isFloat?*double:*int
 
 struct Array
 {
 	void* buffer;
 	int count;
 	int element_size;
+	int isFloat;
 };
+
+
 
 void set_element(struct Array* a, int index, void* value) {
 	void* p = (void*)((char*)a->buffer + a->element_size * index);
@@ -31,23 +36,53 @@ struct Array* CreateInt(int count) {
 }
 
 struct Array* CreateIntegerLinearForm(int length, int* values) {
-	int t = 0;
-	struct Array* form = Create(length, sizeof(int), &t);
+	int zero = 0;
+	struct Array* form = Create(length, sizeof(int), &zero);
 	for (int i = 0; i < form->count; i++) {
-		set_element(form, i, values+i);
+		set_element(form, i, values + i);
 	}
+	form->isFloat = 0;
+	return form;
+}
+
+struct Array* CreateFloatLinearForm(int length, double* values) {
+	int zero = 0;
+	struct Array* form = Create(length, sizeof(double), &zero);
+	for (int i = 0; i < form->count; i++) {
+		set_element(form, i, values + i);
+	}
+	form->isFloat = 1;
 	return form;
 }
 
 void PrintLinearForm(struct Array* form) {
-	for (int i = 0; i < form->count; i++) {
-		if (((int*)form->buffer)[i]) {
-			if (i == 0)
-				printf("%d", ((int*)form->buffer)[i]);
-			else printf("%d*x%d", ((int*)form->buffer)[i], i);
-			if (i != form->count - 1)printf(" + ");
+
+
+	if (form->isFloat) {
+		for (int i = 0; i < form->count; i++) {
+			if (((double*)form->buffer)[i]) {
+				if (i == 0)
+					printf("%lg", ((double*)form->buffer)[i]);
+				else printf("%lg*x%d", ((double*)form->buffer)[i], i);
+				if (i != form->count - 1)printf(" + ");
+			}
 		}
 	}
+	else {
+		for (int i = 0; i < form->count; i++) {
+			if (((int*)form->buffer)[i]) {
+				
+				if (i == 0)
+					printf("%d", ((int*)form->buffer)[i]);
+				else printf("%d*x%d", ((int*)form->buffer)[i], i);
+				if (i != form->count - 1)printf(" + ");
+			}
+		}
+	}
+	
+}
+
+void MultiplyBy(struct Array* form, double number) {
 	
 }
 
@@ -55,19 +90,36 @@ int main() {
 	char* locale = setlocale(LC_ALL, "");
 
 	int len = 0;
-	int* values;
-	printf("¬ведите количество переменных\n");
-	do {
-		scanf("%d", &len);
-	} while (len < 1);
+	char* command = malloc(10*sizeof(char));
+
+	printf("¬ведите команду\n");
+	scanf("%s %d", command, &len);
+
 	len++;
-	values = (int*)malloc(len * sizeof(int));
-	printf("¬ведите коэффициенты (через пробел)\n");
-	for (int i = 0; i < len; i++) {
-		scanf("%d", values+i);
+
+	
+	if (strcmp(command, "IntForm") == 0) {
+		int* values;
+		values = (int*)malloc(len * sizeof(int));
+		printf("¬ведите коэффициенты (через пробел)\n");
+		for (int i = 0; i < len; i++) {
+			scanf("%d", values + i);
+		}
+		printf("\n");
+		struct Array* a = CreateIntegerLinearForm(len, values);
+		PrintLinearForm(a);
 	}
-	printf("\n");
-	struct Array* a = CreateIntegerLinearForm(len, values);
-	PrintLinearForm(a);
+
+	if (strcmp(command, "FloatForm") == 0) {
+		double* values;
+		values = (double*)malloc(len * sizeof(double));
+		printf("¬ведите коэффициенты (через пробел)\n");
+		for (int i = 0; i < len; i++) {
+			scanf("%lf", values + i);
+		}
+		printf("\n");
+		struct Array* a = CreateFloatLinearForm(len, values);
+		PrintLinearForm(a);
+	}
 	return 0;
 }
